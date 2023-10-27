92d004646f6529b8e3bdf9c6326c1cedcf854012
Stearsio <82935372+Stearsio@users.noreply.github.com>
2023-10-27 13:38:55 +0100

    Update jira_connection_config.py

--- jira_connection_config.py
+++ jira_connection_config.py
@@ -4,13 +4,13 @@
 from jira import JIRA
 
 #Type project Key Here
-proj_key = 'DWPPT'
+proj_key = 'project key'
 
 
 def connect_to_jira():
     options = {
-        'server': 'https://hiscox.atlassian.net'
+        'server': 'server'
     }
     #Put your jira account email, and api key in quote marks
-    jira = JIRA(options, basic_auth=('sam.stears@hiscox.com', 'ATATT3xFfGF0Q_KWS91lhvpiAY79ocvpcnP4JjTKVRAuCDLWvFS71SArFgwEhtQ19RosPysRa77g2aa6UX8gkOqs8ROP7CdwfVqp3780z9TJ2rRyk1fY3QiwROrT2DxGCxVqMhhZz7j9-NX-xl98AhgDwqabjWWgvrUu6dF-DSn9liHGxyoCabI=0DF99352'))
+    jira = JIRA(options, basic_auth=('email', 'api key'))
     return jira
